a378eed24416d1094bda145658fe1932abc39d16
Michael Lennon <kali.mist@gmail.com>
2017-10-09 12:01:21 +0100

    Validation Summary Showing

--- MvcIdentityAdapt/MvcIdentityAdapt/Models/UserViewModel.cs
+++ MvcIdentityAdapt/MvcIdentityAdapt/Models/UserViewModel.cs
@@ -10,14 +10,21 @@ namespace MvcIdentityAdapt.Models
     public class UserViewModel
     {
         public string Id { get; set; }
+        [Required]
         public string UserName { get; set; }
+        [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
         [Display(Name = "Confirm Password")]
         [DataType(DataType.Password)]
+        [Compare("Password")]
         public string ConfirmPassword { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public List<SelectListItem> ApplicationRoles { get; set; }
         [Display(Name = "Role")]
         public string ApplicationRoleId { get; set; }
